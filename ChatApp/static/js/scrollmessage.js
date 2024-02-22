40a1b41b507bdd7cf71b4f54bd50787a9a3534e1
yuto <sakonyuuto@sakonyuutonoMacBook-Air.local>
2024-02-22 23:46:52 +0900

    3回目でーす

---
+++ ChatApp/static/js/scrollmessage.js
@@ -0,0 +1,11 @@
+// window.onload = function () {
+//   const scrollmessage = document.getElementById("messagearea");
+//   // 要素内の最下部にスクロール
+//   scrollmessage.scrollTop = scrollmessage.scrollHeight;
+// };
+
+window.addEventListener('DOMContentLoaded',( ) => {
+    const scrollmessage = document.getElementById("messagearea");
+  // 要素内の最下部にスクロール
+  scrollmessage.scrollTop = scrollmessage.scrollHeight;
+})
